3d09f21caf3fe14d1178656ebb80f51f3a339076
AbuBakr <Abubakar.59132@gmail.com>
2025-07-23 22:34:58 +0500

    Completed cart crud.

---
+++ Learnings.Application/Dtos/RolesDto/CartItemDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnings.Application.Dtos.RolesDto
+{
+    public class CartItemDto
+    {
+        public Guid CartItemId { get; set; }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+    public class CartDto
+    {
+        public Guid CartId { get; set; }
+        public string UserId { get; set; }
+        public List<CartItemDto> Items { get; set; } = new();
+        public decimal Total =>
+            Items.Sum(i => i.Price * i.Quantity);
+    }
+
+    public class AddCartItemRequest
+    {
+        [Required] public Guid ProductId { get; set; }
+        [Required][Range(1, int.MaxValue)] public int Quantity { get; set; }
+    }
+
+    public class UpdateCartItemRequest
+    {
+        [Required] public Guid CartItemId { get; set; }
+        [Required][Range(1, int.MaxValue)] public int Quantity { get; set; }
+    }
+
+}
---
+++ Learnings.Application/Services/Interface/ICartService.cs
@@ -0,0 +1,20 @@
+﻿using Learnings.Application.Dtos.RolesDto;
+using Learnings.Application.ResponseBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnings.Application.Services.Interface
+{
+    public interface ICartService
+    {
+        Task<ResponseBase<CartDto>> GetCartByUserAsync(string userId);
+        Task<ResponseBase<CartDto>> AddItemAsync(string userId, AddCartItemRequest req);
+        Task<ResponseBase<CartDto>> UpdateItemAsync(string userId, UpdateCartItemRequest req);
+        Task<ResponseBase<string>> RemoveItemAsync(string userId, Guid cartItemId);
+        Task<ResponseBase<string>> ClearCartAsync(string userId);
+    }
+
+}
